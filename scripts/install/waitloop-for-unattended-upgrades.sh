af64ba84117fc28990c2ef3d80a1c461072ae673
Christian Kim Werner <christiankimwerner@gmail.com>
2016-08-16 19:55:19 +0200

    Replaced own script with way better solution from https://gist.github.com/gretel/34008d667a8a243a9682e5207619ad95

--- scripts/install/waitloop-for-unattended-upgrades.sh
+++ scripts/install/waitloop-for-unattended-upgrades.sh
@@ -1,12 +1,26 @@
-#!/bin/bash
-sleep 30
-isRunning=$(pgrep -cf "apt-get|dpkg")
-echo "$isRunning"
-echo '[INFO] Checking for unattended upgrades to complete'
-while [ "$isRunning" -gt 0 ] 
-do
-echo "E: dpkg or apt-get are still running."
-sleep 0.5
-done
-sudo /bin/cp -f /home/vagrant/10periodic /etc/apt/apt.conf.d/10periodic
-echo '[INFO] Check completed'
+#!/usr/bin/env bash
+# https://gist.github.com/gretel/34008d667a8a243a9682e5207619ad95
+# 2016 tom hensel <github@jitter.eu>
+# `vagrant` on `ubuntu-16.04` can get in conflict with *unattended-upgrade* running and locking the `dpkg` subsystem. this script waits gracefully
+# in `Vagrantfile`:
+#  config.vm.provision 'Wait for unattended-upgrades', type: 'shell', path: './provisioning/wait_unattended_upgrades.sh', args: %w( dpkg apt unattended-upgrade )
+#
+
+function wait_procnames {
+    while true; do
+        alive_pids=()
+        for pname in "$@"; do
+            if [ "$(pgrep "$pname")" ]; then
+                alive_pids+=("$pname ")
+            fi
+        done
+        if [ "${#alive_pids[@]}" -eq 0 ]; then
+            break
+        else
+            printf "waiting for: %s\n" "${alive_pids[@]}"
+        fi
+        sleep 1
+    done
+}
+
+wait_procnames dpkg apt unattended-upgrade
