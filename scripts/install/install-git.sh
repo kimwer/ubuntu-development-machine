e65c805b8f84df3187489ab5947764e91490839c
Christian Kim Werner <christiankimwerner@gmail.com>
2016-08-16 13:24:19 +0200

    Revert "ppa for latest git release doesnt provide a gpg key for xenial atm, so alternatives would be whether allow installing unauthenticated package or use the outdated standard repo. will go for the standard repo solution for obvious security concerns..."
    This reverts commit 5a22b00e50400fc36ca8643b4f3c917a385791fe.

--- scripts/install/install-git.sh
+++ scripts/install/install-git.sh
@@ -1,4 +1,4 @@
-echo '[INFO] Install Git'
-#sudo add-apt-repository ppa:git-core/ppa
-#sudo apt-get update
+echo '[INFO] Install Git from PPA source'
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
 sudo apt-get install git -y
