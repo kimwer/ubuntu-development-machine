c32252eb6538585109169ddeab922b17c9146277
Christian Kim Werner <christiankimwerner@gmail.com>
2016-08-16 16:15:22 +0200

    waitloop script for (more or less) resolve fatal concurrency errors on dpkg database. unattended-updates will set a lock at boot time and prevent provisioning. proper solution would be a custom build ubuntu box see <https://github.com/geerlingguy/packer-ubuntu-1604/issues/3> for further details.

---
+++ scripts/install/waitloop-for-unattended-upgrades.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+isRunning=$(pgrep -cf "apt-get|dpkg")
+echo "$isRunning"
+echo '[INFO] Checking for unattended upgrades to complete'
+while [ "$isRunning" -gt 0 ] 
+do
+echo "E: dpkg or apt-get are still running."
+sleep 0.5
+done
