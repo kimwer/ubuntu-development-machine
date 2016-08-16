99d4f119c7f7dc73465ae59983cd6abaf9aed2c2
Christian Kim Werner <christiankimwerner@gmail.com>
2016-08-16 10:59:33 +0200

    Add nodeJS/NPM installation script

---
+++ scripts/install/install-nodejs.sh
@@ -0,0 +1,5 @@
+echo '[INFO] Install nodejs'
+curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
+sudo apt-get update
+sudo apt-get install build-essential -y
+sudo apt-get install nodejs -y
